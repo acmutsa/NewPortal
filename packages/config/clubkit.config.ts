fa61d5db0a14edfe8161fb80bc4d9ca7b09051b3
Liam Murray <liam@murray.to>
2024-03-13 13:15:49 -0600

    Lay ground work

---
+++ packages/config/clubkit.config.ts
@@ -0,0 +1,14 @@
+export default {
+	clubName: "ClubKit",
+	eventTypes: {
+		ACM: {
+			color: "#179BD5",
+		},
+		"ACM-W": {
+			color: "#7BE9E8",
+		},
+		"Rowdy Creators": {
+			color: "#FFD51E",
+		},
+	},
+} as const;
---
+++ packages/db/schema.ts
@@ -0,0 +1,68 @@
+import {
+	bigserial,
+	text,
+	varchar,
+	uniqueIndex,
+	boolean,
+	timestamp,
+	integer,
+	json,
+	pgEnum,
+	primaryKey,
+	pgTable,
+	PgArray,
+} from "drizzle-orm/pg-core";
+
+// export const userData = pgTable("user_data", {
+// 	race: text("race").array(),
+// });
+
+export const accounts = pgTable("accounts", {
+	clerkID: text("clerk_id").primaryKey(),
+});
+
+export const users = pgTable("users", {
+	memberID: text("member_id").primaryKey(),
+	firstName: text("first_name").notNull(),
+	lastName: text("last_name").notNull(),
+	email: text("email").notNull().unique(),
+	interestedEventTypes: text("interested_event_types").array().notNull(),
+	role: text("role").notNull().default("member"),
+});
+
+export const data = pgTable("data", {
+	major: text("major").notNull(),
+	universityID: text("short_id").notNull().unique(),
+	classification: text("classification").notNull(),
+	graduation: timestamp("graduation").notNull(),
+	birthday: timestamp("birthday").notNull(),
+	gender: text("gender").array().notNull(),
+	ethnicity: text("ethnicity").array().notNull(),
+	resume: text("resume").notNull(),
+	shirtType: text("shirt_size").notNull(),
+});
+
+export const eventCategories = pgTable("event_categories", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull().unique(),
+	color: text("color").notNull(),
+});
+
+export const events = pgTable("events", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull(),
+	description: text("description").notNull(),
+	start: timestamp("start").notNull(),
+	end: timestamp("end").notNull(),
+	checkinStart: timestamp("checkin_start").notNull(),
+	checkinEnd: timestamp("checkin_end").notNull(),
+	location: text("location").notNull(),
+	isUserCheckinable: boolean("is_user_checkinable").notNull().default(true),
+});
+
+export const checkins = pgTable("checkins", {
+	eventID: text("event_id").notNull(),
+	memberID: text("member_id").notNull(),
+	time: timestamp("time").defaultNow().notNull(),
+	feedback: text("feedback"),
+});
