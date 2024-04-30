f987b05c51d39f9492070f0c305794f1e283950f
Liam Murray <liam@murray.to>
2024-04-30 15:53:29 -0500

    Fix: with-env

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-	return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+	return (
+		<main className="h-screen w-screen flex items-center justify-center">
+			<h1 className="font-black text-4xl">ClubKit</h1>
+		</main>
+	);
 }
