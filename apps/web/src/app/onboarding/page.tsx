a26476c5a1daa91d55bdb5dd7f90398f478dda29
Liam Murray <liam@murray.to>
2024-05-04 17:08:27 -0500

    Add: onboarding pages

--- apps/web/src/app/onboarding/page.tsx
+++ apps/web/src/app/onboarding/page.tsx
@@ -1,3 +1,28 @@
+import RegisterForm from "@/components/registration/registerForm";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-	return <div>Register</div>;
+	return (
+		<main className="w-screen">
+			<div className="max-w-5xl mx-auto min-h-screen pt-40">
+				<div className="grid grid-cols-2">
+					<div>
+						<h1 className="font-black text-5xl">Registration</h1>
+						<p className="mt-5 font-medium">
+							<span className="font-bold">Welcome!</span> Please fill out the form
+							below to complete your registration.
+						</p>
+					</div>
+					<div className="bg-primary rounded-lg text-white flex flex-col items-center justify-center gap-y-3">
+						<p className="text-sm font-bold">Had a portal (abc123 & email) account?</p>
+						<Link href="/onboarding/migrate">
+							<Button className="w-full dark">Migrate from Portal</Button>
+						</Link>
+					</div>
+				</div>
+				<RegisterForm />
+			</div>
+		</main>
+	);
 }
---
+++ apps/web/src/components/registration/registerForm.tsx
@@ -0,0 +1,13 @@
+"use client";
+import c from "config";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function RegisterForm() {
+	return (
+		<div className="mt-20">
+			<p>cha cha real smooth</p>
+		</div>
+	);
+}
