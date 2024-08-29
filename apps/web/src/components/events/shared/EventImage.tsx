63325a7be004e9e4e4da395e877c30f362ccb7b5
Christian Walker <christiandwalker2021@gmail.com>
2024-08-29 11:02:29 -0500

    Add: Eventimage wrapper

---
+++ apps/web/src/components/events/shared/EventImage.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Image from "next/image";
+import type { EventImageProps } from "@/lib/types/events";
+import { useState,useEffect } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function EventImage(props:EventImageProps){
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(()=>{
+    console.log("loaded is: ", loaded)
+  },[loaded])
+
+  const combinedProps = {
+		alt: "Event Image",
+		priority: true,
+    width:0,
+    height:0,
+    quality:100,
+		sizes:"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw",
+    ...props
+  };
+
+  return (
+		<>
+			<Image
+				{...combinedProps}
+				onLoad={() => setLoaded(true)}
+        hidden={!loaded}
+			/>
+			<Skeleton className={`${loaded ? "hidden" : "h-60 w-full"}`} hidden={loaded} />
+		</>
+  );
+}
