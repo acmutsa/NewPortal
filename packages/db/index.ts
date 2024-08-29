352ffceb25be30ed2fb2b0a5033c8f8eecaa0ce6
Christian Walker <christiandwalker2021@gmail.com>
2024-08-29 11:01:48 -0500

    Chore: remove excess comments

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,5 @@
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
-// import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 import * as schema from "./schema";
 
 export { union } from "drizzle-orm/pg-core";
