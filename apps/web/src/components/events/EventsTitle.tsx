f0d6325c18906a6ca6ce39a88d142119e2706bb8
Christian Walker <christiandwalker2021@gmail.com>
2024-09-07 21:37:53 -0500

    updates event title

--- apps/web/src/components/events/EventsTitle.tsx
+++ apps/web/src/components/events/EventsTitle.tsx
@@ -5,7 +5,7 @@ export default function EventsTitle() {
 			<h1 className="pt-2 text-center text-5xl font-black md:text-6xl ">
 				Events
 			</h1>
-			<h3 className="text-center font-bold sm:text-xl">
+			<h3 className="text-center font-bold sm:text-xl 2xl:text-2xl">
 				Check Out What {c.clubName} Has Planned!
 			</h3>
 		</div>
