41119e3e705de08bd37eb28a338a0a55b650c067
Christian Walker <christiandwalker2021@gmail.com>
2024-09-09 23:43:50 -0500

    add loading state

--- apps/web/src/app/dash/page.tsx
+++ apps/web/src/app/dash/page.tsx
@@ -4,6 +4,8 @@ import { VERCEL_IP_TIMEZONE_HEADER_KEY } from "@/lib/constants";
 import UserDash from "@/components/dash/UserDash";
 import { headers } from "next/headers";
 import { getClientTimeZone } from "@/lib/utils";
+import { Suspense } from "react";
+import { LoaderCircle } from "lucide-react";
 export default function Page() {
 	const { userId } = auth();
 
@@ -14,7 +16,13 @@ export default function Page() {
 	
 	return (
 		<main className="flex min-h-[calc(100vh-4rem)] w-screen items-center justify-center overflow-x-hidden px-4 py-4 md:px-5 ">
-			<UserDash userId={userId} clientTimeZone={clientTimeZone} />
+			 <Suspense fallback={
+				<div className="flex flex-col items-center space-y-2">
+				<LoaderCircle className="h-auto w-7 lg:w-8 xl:w-9 animate-spin" />
+				<h3>Loading. Please wait.</h3>
+			</div>}>
+				<UserDash userId={userId} clientTimeZone={clientTimeZone} />
+			</Suspense> 
 		</main>
 	);
 }
