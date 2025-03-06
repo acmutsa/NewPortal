86b69e72521833cb224e2c9dd7fcbb67a1d535a7
Christian Walker <christiandwalker2021@gmail.com>
2025-03-05 20:10:53 -0600

    adds settings to mobile nav

--- apps/web/src/components/shared/navbar.tsx
+++ apps/web/src/components/shared/navbar.tsx
@@ -125,6 +125,13 @@ export default async function Navbar({ siteRegion, showBorder }: NavbarProps) {
 					<SheetContent className="flex max-w-[40%] flex-col-reverse items-center justify-center gap-y-1">
 						{user ? (
 							<>
+							{
+								registrationComplete && (
+									<Link href="/settings">
+										<Button variant="ghost">Settings</Button>
+									</Link>
+								)
+							}
 								<Link
 									href={
 										registrationComplete
