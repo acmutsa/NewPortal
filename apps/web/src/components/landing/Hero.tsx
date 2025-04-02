bdcfedec48244f16dc383581ab58bcdc9d8a546b
joshuasilva414 <joshuasilva414@gmail.com>
2025-04-02 08:58:55 -0500

    Revert "kailey-ified"
    This reverts commit eb44c1517b5a44a7960a027015766f5ae7fe67b3.

--- apps/web/src/components/landing/Hero.tsx
+++ apps/web/src/components/landing/Hero.tsx
@@ -1,54 +1,10 @@
-'use client'
 import React from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { useState, useEffect } from "react";
-import {motion} from "framer-motion"
-import Image from "next/image";
 
 export default function Hero() {
-	const [isVisible, setIsVisible] = useState(false);
-	const [innerWidth, setInnerWidth] = useState(0);
-	const [innerHeight, setInnerHeight] = useState(0);
-
-	useEffect(() => {
-		setIsVisible(true);
-		setInnerWidth(window.innerWidth);
-		setInnerHeight(window.innerHeight);
-	}, []);
-
-
-
 	return (
-		<section className="bg-hero relative h-full flex-1">
-			<div className="absolute inset-0 overflow-hidden">
-				{Array.from({ length: 20 }).map((_, i) => (
-					<motion.div
-						key={i}
-						className="absolute opacity-30"
-						initial={{ opacity: 0, scale: 0 }}
-						animate={{
-							opacity: [0.2, 0.5, 0.2],
-							scale: [1, 2, 1],
-							x: [
-								Math.random() * innerWidth,
-								Math.random() * innerWidth,
-							],
-							y: [
-								Math.random() * innerHeight,
-								Math.random() * innerHeight,
-							],
-						}}
-						transition={{
-							duration: Math.random() * 10 + 10,
-							repeat: Infinity,
-							delay: Math.random() * 5,
-						}}
-					>
-						<Image src="/img/logos/kailey-float.png" width={20} height={20} alt="turtle"/>
-					</motion.div>
-				))}
-			</div>
+		<section className="bg-hero h-full flex-1">
 			<div className="flex h-full flex-col items-center gap-y-20">
 				<div className="mt-20 px-8">
 					<h1 className="text-center font-acm text-7xl font-bold leading-9 text-white lg:text-title">
